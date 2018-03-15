16710f3ae5503f393c3c40779fee5ba87d0d0ac3
LORD KAY <offeilord@gmail.com>
2018-03-15 14:02:15 +0000

    Add the version prompt

--- installer.sh
+++ installer.sh
@@ -9,7 +9,21 @@ package_name=$package;
 if [[ -z "$package_name" ]]; then
 	# Get the current working directory name
 	current_folder=${PWD##*/};
-	echo $current_folder;
+	sleep 2
+	mkdir $current_folder;
+	chmod 777 $current_folder;
+else
+	sleep 2
+	mkdir $package_name;
+	chmod 777 $package_name;
+fi
+
+read -p "Package Version (v1.0.0) : -> " version;
+version_number=$version;
+if [[ -z "$version_number" ]]; then
+	version_number="v1.0.0";
+else
+	version=$version_number;
 fi
 
 
