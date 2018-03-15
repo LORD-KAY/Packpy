1deee015f4bb8ef8f53988d8f01ab4de4123af4a
LORD KAY <offeilord@gmail.com>
2018-03-15 14:12:01 +0000

    move to the current folder

--- installer.sh
+++ installer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 APPNAME="Packpy v1.0.0";
 HOSTNAME=`hostname`;
+INIT_FILE="__init__.py";
 
 # Begining the installer by processing using user argument - Package Name
 read -p "Enter Your Package Name: -> " package;
@@ -12,16 +13,24 @@ if [[ -z "$package_name" ]]; then
 	sleep 2
 	mkdir $current_folder;
 	chmod 777 $current_folder;
+	cd $current_folder;
 else
 	sleep 2
 	mkdir $package_name;
 	chmod 777 $package_name;
+	cd $current_folder;
 fi
 
 read -p "Package Version (v1.0.0) : -> " version;
 version_number=$version;
 if [[ -z "$version_number" ]]; then
 	version_number="v1.0.0";
+
+	# Writing to the init file
+	# header="
+	# from __future__ import absolute_import,division, print_function
+	# ";
+	# echo "$header" > $INIT_FILE;
 else
 	version=$version_number;
 fi
