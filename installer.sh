9f32ceef05c27118281b39e9e236f6ca5d2d4620
LORD KAY <offeilord@gmail.com>
2018-03-15 13:39:11 +0000

    Add first code for package name

--- installer.sh
+++ installer.sh
@@ -2,3 +2,16 @@
 APPNAME="Packpy v1.0.0";
 HOSTNAME=`hostname`;
 
+# Begining the installer by processing using user argument - Package Name
+read -p "Enter Your Package Name: -> " package;
+package_name=$package;
+# Creating the package folder
+if [[ $package_name ==  ' ' ]]; then
+	
+fi
+sleep 2
+mkdir $package_name;
+chmod 777 $package_name;
+
+read -p ""
+
