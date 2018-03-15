1fb6ee25205e879950dba7a4d5e10e3221b5beb3
LORD KAY <offeilord@gmail.com>
2018-03-15 14:53:41 +0000

    Define all the extra variables and the neccessary spaces

--- installer.sh
+++ installer.sh
@@ -29,14 +29,48 @@ if [[ -z "$package_name" ]]; then
 		# Writing to the init file
 		file_header="#!/bin/env/python";
 		imports="from __future__ import absolute_import,division, print_function";
-		all_configs='
-		__all__ = (
+		all_configs='__all__ = (
     "__title__", "__summary__", "__uri__", "__version__", "__author__",
     "__email__", "__license__", "__copyright__",
 	)';
+
 		echo "$file_header" >> $INIT_FILE;
 		echo "$imports" >> $INIT_FILE;
 		echo "$all_configs" >> $INIT_FILE;
+
+		# Writing other information to the init file
+		title="$current_folder";
+		echo " " >> $INIT_FILE;
+		echo "__title__ = $title" >> $INIT_FILE;
+		read -p "Package Summary :-> " summary;
+		summary_data=$summary;
+		echo "__summary__ = $summary_data" >> $INIT_FILE;
+		read -p "Package URI (Mostly github repository): -> " uri;
+		url=$uri;
+		echo "__uri__ = $url" >> $INIT_FILE;
+		# DEFINING AN EMPTY SPACE ABOVE
+		echo " " >> $INIT_FILE;
+		echo "__version__ = $version_number" >> $INIT_FILE;
+		echo " " >> $INIT_FILE;
+
+		read -p "Package Author: -> " author;
+		devname=$author;
+		echo "__uri__ = $devname" >> $INIT_FILE;
+		read -p "Author Email: -> " email;
+		email_address=$emai;
+		echo "__email__ = $email" >> $INIT_FILE;
+		read -p "Licence Under: -> " license;
+		if [[ -z "$license" ]]; then
+			license="MIT";
+			echo " " >> $INIT_FILE;
+			echo "__license__ = $license" >> $INIT_FILE;
+			echo " " >> $INIT_FILE;
+		else
+			echo " " >> $INIT_FILE;
+			echo "__license__ = $license" >> $INIT_FILE;
+			echo " " >> $INIT_FILE;
+		fi
+		
 	
 		# CREATING OTHER NECCESSARY FILES
 		touch $MANIFEST && chmod 755 $MANIFEST;
@@ -47,7 +81,7 @@ if [[ -z "$package_name" ]]; then
 		# Create the package folder and the its file using the same package folder name
 		mkdir $current_folder;
 		cd $current_folder && touch "$current_folder.py";
-		chmod "$current_folder.py";
+		chmod 755 "$current_folder.py";
 	else
 		version=$version_number;
 	fi
